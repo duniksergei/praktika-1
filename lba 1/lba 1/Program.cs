d0f38072526fdb7da2cffa44c801564ac36d8ef3
dunikserei <dunik99@mail.ru>
2018-04-23 18:24:30 +0300

    pervi

---
+++ lba 1/lba 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lba_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                    //вывод информации в консоль
+                    Console.WriteLine("---------------------------------------------- ");
+                    Console.WriteLine("Введите информацию о Студенте: ");
+                    Console.WriteLine("---------------------------------------------- ");
+                    Student st1 = new Student(); //создание объекта класса
+                    st1.Input();      //вызов метода ввода класса Student
+
+                    Console.Clear(); //очищение консоли
+
+                    //вывод информации в консоль
+                    Console.WriteLine("----------------------------------------------- ");
+                    st1.Display();     //вызов метода вывода класса Student
+            Console.WriteLine("----------------------------------------------- ");
+
+                    Console.ReadKey();  //ожидание нажатия клавиши для завершение программы
+
+                }
+            
+        }
+
+    }
+
---
+++ lba 1/lba 1/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lba_1
+{
+    class Student
+    {
+        /*private string name;
+        private int kurs;
+        private bool sex;*/
+        //свойства класса
+        public string Name { get; set; }         //свойство имя студента
+        public int Kurs { get; set; }       //свойство курса
+        public bool Sex { get; set; }          //свойство пола
+        public Student()  //конструктор по умолчанию
+        {
+            Name = null;
+            Kurs = 0;
+            Sex = true;
+        }
+        public Student(string Name, int Kurs, bool Sex) //конструктор с параметрами
+        {
+            this.Name = Name;
+            this.Kurs = Kurs;
+            this.Sex = Sex;
+        }
+        public virtual void Display() //метод для вывода 
+        {
+            Console.WriteLine("Имя студента: " + Name);
+            Console.WriteLine("Курс: " + Kurs);
+            Console.WriteLine("Пол: " + Sex);
+        }
+        public virtual void Input()  //метод для ввода
+        {
+            Console.WriteLine("Имя студента:  ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Курс: ");
+            Kurs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Пол: ");
+            Sex = Convert.ToBoolean(Console.ReadLine());
+
+        }
+    }
+}
